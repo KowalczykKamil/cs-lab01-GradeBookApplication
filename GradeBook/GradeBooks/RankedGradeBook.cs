ee0a8923c5c30d7e80d5b1548f51d866011a06d6
KowalczykKamil <kamil.kowalczyk1@microsoft.wsei.edu.pl>
2022-03-31 12:30:07 +0200

    CalculateStatistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -4,9 +4,9 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    public class RankedGradeBook:BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradeBook(string name):base(name)
+        public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
         }
@@ -26,6 +26,17 @@ namespace GradeBook.GradeBooks
                 return 'D';
             else
                 return 'F';
+        }
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
         }
     }
 }
